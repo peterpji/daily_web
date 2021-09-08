fa503bc18588bc2f615db2032da00648f901dfe9
peterpji <peterpji@hotmail.com>
2021-09-08 09:20:48 +0300

    Slight refactoring

--- DailyWeb.py
+++ DailyWeb.py
@@ -27,28 +27,30 @@ def open_web_links_in_file(path_link_file: str, heading_links_to_open: Optional[
                 open_if_is_link(row)
 
 
-def get_last_execution_date() -> str:
-    if os.path.exists(PATH_LAST_EXECUTION_TIME):
-        with open(PATH_LAST_EXECUTION_TIME) as f:
-            last_execution_date = f.readline()
-    else:
-        last_execution_date = None
-
-    return last_execution_date
-
-
 def update_last_execution_date(new_date: str):
     with open(PATH_LAST_EXECUTION_TIME, 'w') as f:
         f.write(new_date)
 
 
+def is_not_opened_today(today):
+    def get_last_execution_date() -> str:
+        if os.path.exists(PATH_LAST_EXECUTION_TIME):
+            with open(PATH_LAST_EXECUTION_TIME) as f:
+                last_execution_date = f.readline()
+        else:
+            last_execution_date = None
+
+        return last_execution_date
+
+    last_execution_date = get_last_execution_date()
+    return today != last_execution_date
+
+
 def main():
     open_web_links_in_file(PATH_LINKS_TO_OPEN, heading_links_to_open='# Always')
 
-    last_execution_date = get_last_execution_date()
     today = datetime.datetime.now().strftime('%Y-%m-%d')
-
-    if today != last_execution_date:
+    if is_not_opened_today(today):
         open_web_links_in_file(PATH_LINKS_TO_OPEN, heading_links_to_open='# Daily')
 
     update_last_execution_date(today)
